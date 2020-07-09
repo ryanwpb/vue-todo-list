84aa3d2381fda73177a571b9a000ae09ef9ac572
Ryan Graham <ryangrahamwpb@gmail.com>
2020-07-09 09:17:03 -0400

    Add todo.

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo-item">
+    <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+};
+</script>
+
+<style scoped>
+.todo-item {
+  padding: 1rem;
+}
+p {
+  color: #4e6e8e;
+}
+</style>
